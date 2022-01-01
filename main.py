4d3a80fb043bebc3c4a55d8a985e82f4f1d0442b
TN Botz <77948100+rubandurai27@users.noreply.github.com>
2022-01-01 17:53:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ Bot = Client(
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"],
-    token = os.environ["TOKEN"]
+    token = int(os.environ["TOKEN"]
 )
 
 
